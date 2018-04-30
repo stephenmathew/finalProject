bc1675904036aac6c14dac193ae1e10ab6528370
achesky <afc340@nyu.edu>
2018-04-30 14:27:03 +0800

    perfected text & added file to GitHub
    nailed it

--- text_attempt1-AC.py
+++ text_attempt1-AC.py
@@ -4,17 +4,16 @@
 authors = [ ' ' for x in range (0,242)]
 author_id = [ ' ' for x in range(0,242)]
 author_name = [ ' ' for x in range(0,242)]
-post = [' ' for x in range(0,1076)]
+post = [' ' for x in range(0,1079)]
 trying = [ ' ' for x in range(0,50)]
 num_articles = 0
 
-f = open("/Users/achesky/Downloads/oncenturyavenue.wordpress.2018-03-31.xml", "r")
+f = open("oncenturyavenue.wordpress.2018-03-31.xml", "r")
 for i in range(0,37):
     line =f.readline()
 for i in range(0,242):
     line = f.readline()
     authors[i] = line
-g = open("/Users/achesky/Desktop/ocatextposts.xml", "w")
 save = ''
 j = -1
 for i in range(0,200030):
@@ -26,78 +25,59 @@ for i in range(0,200030):
     post[j]+= line
     #splitting file into posts
 
+post[1076] = "let's see"
+post[1077] = "let's see"
+post[1078] = "let's see"
 f.close()
 
 from bs4 import  BeautifulSoup
 import re
-"""
-print(post[3])
-print('practice')
-source_code = post[3]
-soup = BeautifulSoup(source_code, "lxml")
-
-tag = soup.title
-tag.clear()
-tag = soup.link
-tag.clear()
-hold = soup.get_text()
-print('test')
-print(hold)
-"""
 
 
+
+print(post[-1])
 for i in range(0,len(post)):
-    print(i)
-    print(post[i])
+    source_code = post[i]
+    soup = BeautifulSoup(source_code, "xml")
+    j = 0
+    hold = [' ' for x in range(0,2)]
+    for encoded_content in soup.findAll("encoded"):
+        for child in encoded_content.children:
+            hold[j] = child
+            print(child)
+            j += 1
+    post[i] = hold[0]
+
     source_code = post[i]
     soup = BeautifulSoup(source_code, "lxml")
-    hold = soup.find_all('p')
-    print('test')
-    if hold == []:
-        source_code = post[i]
-        soup = BeautifulSoup(source_code, "lxml")
-        """
-        tag = soup.title
-        tag.clear()
-        tag = soup.link
-        tag.clear()
-        """
-        hold = soup.get_text()
-        post[i] = hold
-        print(post[i])
-    else: 
-        post[i] = ''
-        for j in hold:
-            post[i] += str(j) + ' '
-        
-        start = [m.start() for m in re.finditer('<', post[i])]
-        end = [m.start() for m in re.finditer('>', post[i])]
-        
-        hold = post[i]
-        post[i] = ''
-        if start[0] == 0:
+    start = [m.start() for m in re.finditer('<', post[i])]
+    end = [m.start() for m in re.finditer('>', post[i])]
+    
+    hold = post[i]
+    post[i] = ''
+    print(hold)
+    print(start)
+    if start == []:
+        continue;
+    elif start[0] == 0:
             for j in range(0,(len(start)-1)):
-                post[0] += hold[end[j]:start[j+1]]
-            post[0] = post[0].replace('>', ' ')
-        else:
-            print('yikes')
-        print(post[i])
-
-
-
-
-
-
-
-
-
-
-"""
-print(post)
-
+                post[i] += hold[end[j]:start[j+1]]
+            post[i] = post[i].replace('>', ' ')
+    else:
+        print('yikes')
+        hold = BeautifulSoup(hold, "lxml")
+        post[i] = hold.get_text()
+print(post[-1])
+g = open("ocatextposts.txt", "w")
+for i in range(0,len(post)):
+    g.write(str(i))
+    g.write('\n')
+    g.write(post[i])
+    g.write('\n')
+g.close()
 
 ### Create dictionary with key as author_id and name as value
-f = open("/Users/achesky/Downloads/oncenturyavenue.wordpress.2018-03-31.xml", "r")
+f = open("oncenturyavenue.wordpress.2018-03-31.xml", "r")
 for i in range(0,113):
     line = f.readline()
     line = line[0:7]
@@ -132,18 +112,3 @@ for i in range(len(author_id)):
     authors_dict[author_id[i]] = author_name[i]
 print(authors_dict)
 f.close()
-
-"""
-"""
-for i in range(0,37440):
-while True:
-    line = f.readline()
-    if len(line)==0:
-        break
-    line = line[:-1]
-    line = line.split(',')
-    x_input[i] = line
-    x_input[i] = [float(x) for x in x_input[i] ]
-    i += 1
-f.close()
-"""
